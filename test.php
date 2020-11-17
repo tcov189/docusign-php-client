b77a29930061fb4a60f7346b45365cbd01ed3d1a
Trevor Covington <tcovington189@gmail.com>
2020-11-17 09:33:07 -0600

    Add another passing example

--- test.php
+++ test.php
@@ -5,12 +5,17 @@ require './vendor/autoload.php';
 // Run php test.php
 
 // Returns 1
-$guzzle = class_exists('\Guzzle\Http\Client') ? 1 : 0;
+$guzzle = class_exists('Guzzle\Http\Client') ? 1 : 0;
 echo $guzzle;
 echo PHP_EOL;
 
+// Returns 1
+$phpCsFixer = class_exists('PhpCsFixer\Config') ? 1 : 0;
+echo $phpCsFixer;
+echo PHP_EOL;
+
 // Return 0
-$docusign1 = class_exists('\DocuSign\eSign\Api\AccountsApi\CreateAccountSignaturesOptions') ? 1 : 0;
+$docusign1 = class_exists('DocuSign\eSign\Api\AccountsApi\CreateAccountSignaturesOptions') ? 1 : 0;
 echo $docusign1;
 echo PHP_EOL;
 
